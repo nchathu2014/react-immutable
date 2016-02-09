3c2474c592fabdc07b26166f30ad80ee31ad553c
Chathuranga <UCHATNU@PEROOT.COM>
2016-02-09 13:12:29 +0530

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {List,Map} from 'immutable';
+
+export default class App extends React.Component{
+
+	constructor(){
+		super();
+
+		this.state={
+			
+		}
+	}
+
+	render(){
+		return(
+			<div>
+				<h2>Immutable data and ReactJS</h2>
+			</div>
+		);
+	}
+}
